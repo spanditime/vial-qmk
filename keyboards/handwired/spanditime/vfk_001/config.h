c1fe665f7ac588385cf96ff08dfcf946050fb1e7
Frolov Eugene <spanditime@gmail.com>
2024-07-03 08:33:45 +0300

    Add config for vfk-001 keyboard

---
+++ keyboards/handwired/spanditime/vfk_001/config.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+// #undef LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+// #undef LOCKING_RESYNC_ENABLE
+
+/* Enables This makes it easier for fast typists to use dual-function keys */
+// #undef PERMISSIVE_HOLD
---
+++ keyboards/handwired/spanditime/vfk_001/rules.mk
@@ -0,0 +1,4 @@
+# This file intentionally left blank
+LTO_ENABLE = yes
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
